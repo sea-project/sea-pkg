1e1880f017fcefbf5536b52e8778f067e0bd0d78
burrow <wangqi@peogoo.com>
2020-09-25 11:04:53 +0800

    test account

--- crypto/galois/shamir_test.go
+++ crypto/galois/shamir_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestSplit(t *testing.T) {
-	split, err := Split([]byte("0ddb327ad1059662da1f02f1b8521bf0f69cf5cecc09a4d8fc7f928fc9726818"), 5, 2)
+	split, err := Split([]byte("0ddb327ad1059662da1f02f1b8521bf0f69cf5cecc09a4d8fc7f928fc9726818"), 4, 2)
 	if err != nil {
 		panic(err)
 	}
